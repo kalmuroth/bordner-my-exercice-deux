afba0aacd568768f261c44bf8b33bf77af205066
kalmuroth <lucas.bordner@cloudartifact.com>
2021-11-05 14:28:14 +0100

    1/2 exo 8

--- exercice-8/index.js
+++ exercice-8/index.js
@@ -1,11 +1,41 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
+class BatailleNavale {
+    constructor() {
+         this.matrice = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0],[0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 4, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+         this.el = document.querySelector("#app");
+         this.run();
+    }
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const francais = loginForm.francais.value;
-    const math = loginForm.math.value;
-    const anglais = loginForm.anglais.value;
-    const sum = (parseInt(francais) + parseInt(math) + parseInt(anglais))/3
-    return sum
-})
+    render() {
+        const table = document.createElement('table');
+        for (let i = 0; i < this.matrice.length; i += 1) {
+          const tr = document.createElement('tr');
+    
+          for (let j = 0; j < this.matrice.length; j += 1) {
+            const td = document.createElement('td');
+            td.style.padding = "20px";
+            td.style.border = "12px solid black";
+            td.style.background = "grey";
+            td.dataset.id = this.matrice[i][j]
+            td.textContent = this.matrice[i][j]
+            tr.appendChild(td);
+            td.addEventListener("click", (e) => {
+                if (e.currentTarget.getAttribute('data-id') === '1'){
+                    e.currentTarget.style.background = "red";
+                }
+                else {
+                    e.currentTarget.style.background = "blue";
+                }
+            });
+          }
+          table.appendChild(tr);
+        }
+    
+        this.el.appendChild(table);
+      }
+
+    run() {
+        this.render();
+    }
+}
+
+const bataille = new BatailleNavale();
